bc9d315a8f8e15b1dab72566ee2b45f8bec55452
chabbirhosen1 <chabbirhosen1@gmail.com>
2024-10-20 21:53:29 +0600

    chabbir

--- index.js
+++ index.js
@@ -1,87 +1,130 @@
-const { spawn } = require("child_process");
-const { readFileSync } = require("fs-extra");
-const http = require("http");
-const axios = require("axios");
-const semver = require("semver");
-const logger = require("./utils/log");
+const assets = require('@miraipr0ject/assets');
+const crypto = require('crypto');
+const os = require("os");
 
-/////////////////////////////////////////////
-//========= Check node.js version =========//
-/////////////////////////////////////////////
+module.exports.throwError = function (command, threadID, messageID) {
+	const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+	return global.client.api.sendMessage(global.getText("utils", "throwError", ((threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX), command), threadID, messageID);
+}
 
-// const nodeVersion = semver.parse(process.version);
-// if (nodeVersion.major < 13) {
-//     logger(`Your Node.js ${process.version} is not supported, it required Node.js 13 to run bot!`, "error");
-//     return process.exit(0);
-// };
+module.exports.cleanAnilistHTML = function (text) {
+	text = text
+		.replace('<br>', '\n')
+		.replace(/<\/?(i|em)>/g, '*')
+		.replace(/<\/?b>/g, '**')
+		.replace(/~!|!~/g, '||')
+		.replace("&amp;", "&")
+		.replace("&lt;", "<")
+		.replace("&gt;", ">")
+		.replace("&quot;", '"')
+		.replace("&#039;", "'");
+	return text;
+}
 
-///////////////////////////////////////////////////////////
-//========= Create website for dashboard/uptime =========//
-///////////////////////////////////////////////////////////
+module.exports.downloadFile = async function (url, path) {
+	const { createWriteStream } = require('fs');
+	const axios = require('axios');
 
-const dashboard = http.createServer(function (_req, res) {
-    res.writeHead(200, "OK", { "Content-Type": "text/plain" });
-    res.write("HI! EVERYONE , THIS BOT WAS MADE BY CHABBIR XOWAD! CONTACT HER ON FACEBOOK: https://mobile.facebook.com/profile.php?id=100083391553064");
-    res.end();
-});
+	const response = await axios({
+		method: 'GET',
+		responseType: 'stream',
+		url
+	});
 
-dashboard.listen(process.env.port || 0);
+	const writer = createWriteStream(path);
 
-logger("Opened server site...", "[ Starting ]");
+	response.data.pipe(writer);
 
-/////////////////////////////////////////////////////////
-//========= Create start bot and make it loop =========//
-/////////////////////////////////////////////////////////
-
-function startBot(message) {
-    (message) ? logger(message, "[ Starting ]") : "";
-
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "mirai.js"], {
-        cwd: __dirname,
-        stdio: "inherit",
-        shell: true
-    });
-
-    child.on("close", (codeExit) => {
-        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
-            startBot("Restarting...");
-            global.countRestart += 1;
-            return;
-        } else return;
-    });
-
-    child.on("error", function (error) {
-        logger("An error occurred: " + JSON.stringify(error), "[ Starting ]");
-    });
+	return new Promise((resolve, reject) => {
+		writer.on('finish', resolve);
+		writer.on('error', reject);
+	});
 };
-////////////////////////////////////////////////
-//========= Check update from Github =========//
-////////////////////////////////////////////////
 
+module.exports.getContent = async function(url) {
+	try {
+		const axios = require("axios");
 
-axios.get("https://raw.githubusercontent.com/d-jukie/miraiv2/main/package.json").then((res) => {
+		const response = await axios({
+			method: 'GET',
+			url
+		});
 
+		const data = response;
 
-});
-startBot();
-/*axios.get("https://raw.githubusercontent.com/d-jukie/miraiv2_fix/main/package.json").then((res) => {
-    const local = JSON.parse(readFileSync('./package.json'));
-    if (semver['lt'](local.version, res['data']['version'])) {
-        if (local.autoUpdate == !![]) {
-            logger('A new update is available, start update processing...', '[ UPDATE ]');
-            const updateBot = {};
-            updateBot.cwd = __dirname
-            updateBot.stdio = 'inherit' 
-            updateBot.shell = !![];
-            const child = spawn('node', ['update.js'], updateBot);
-            child.on('exit', function () {
-                return process.exit(0);
-            })
-            child.on('error', function (error) {
-                logger('Unable to update:' + JSON.stringify(error), '[ CHECK UPDATE ]');
-            });
-        } else logger('A new update is available! Open terminal/cmd and type "node update" to update!', '[ UPDATE ]'), 
-        startBot();
-    } else logger('You are using the latest version!', '[ CHECK UPDATE ]'), startBot();
-}).catch(err => logger("Unable to check update.", "[ CHECK UPDATE ]"));*/
-//
+		return data;
+	} catch (e) { return console.log(e); };
+}
+
+module.exports.randomString = function (length) {
+	var result           = '';
+	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	var charactersLength = characters.length || 5;
+	for ( var i = 0; i < length; i++ ) result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	return result;
+}
+
+module.exports.assets = {
+	async font (name) {
+		if (!assets.font.loaded) await assets.font.load();
+		return assets.font.get(name);
+	},
+	async image (name) {
+		if (!assets.image.loaded) await assets.image.load();
+		return assets.image.get(name);
+	},
+	async data (name) {
+		if (!assets.data.loaded) await assets.data.load();
+		return assets.data.get(name);
+	}
+}
+
+module.exports.AES = {
+	encrypt (cryptKey, crpytIv, plainData) {
+		var encipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(cryptKey), Buffer.from(crpytIv));
+        var encrypted = encipher.update(plainData);
+		encrypted = Buffer.concat([encrypted, encipher.final()]);
+		return encrypted.toString('hex');
+	},
+	decrypt (cryptKey, cryptIv, encrypted) {
+		encrypted = Buffer.from(encrypted, "hex");
+		var decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(cryptKey), Buffer.from(cryptIv, 'binary'));
+		var decrypted = decipher.update(encrypted);
+	
+		decrypted = Buffer.concat([decrypted, decipher.final()]);
+	
+		return String(decrypted);
+	},
+	makeIv () { return Buffer.from(crypto.randomBytes(16)).toString('hex').slice(0, 16); }
+}
+
+module.exports.homeDir = function () {
+	var returnHome, typeSystem;
+	const home = process.env["HOME"];
+	const user = process.env["LOGNAME"] || process.env["USER"] || process.env["LNAME"] || process.env["USERNAME"];
+
+	switch (process.platform) {
+		case "win32": {
+			returnHome = process.env.USERPROFILE || process.env.HOMEDRIVE + process.env.HOMEPATH || home || null;
+			typeSystem = "win32"
+			break;
+		}
+		case "darwin": {
+			returnHome = home || (user ? '/Users/' + user : null);
+			typeSystem = "darwin";
+			break;
+		}
+		case "linux": {
+			returnHome =  home || (process.getuid() === 0 ? '/root' : (user ? '/home/' + user : null));
+			typeSystem = "linux"
+			break;
+		}
+		default: {
+			returnHome = home || null;
+			typeSystem = "unknow"
+			break;
+		}
+	}
+
+	return [typeof os.homedir === 'function' ? os.homedir() : returnHome, typeSystem];
+}
